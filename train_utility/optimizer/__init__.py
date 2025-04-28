7cefb2494d711ed4a720877b4e771d1f28b6480e
FocueAI <17051325951@163.com>
2025-04-28 14:55:59 +0800

    学习率调整策略， 其中StepLR 已经完成

---
+++ train_utility/optimizer/__init__.py
@@ -0,0 +1,23 @@
+import copy
+import torch
+
+__all__ = ['build_optimizer']
+
+def build_optimizer(optim_config, lr_scheduler_config , model):
+    from . import lr
+    
+    # step1: 优化器相关的配置
+    optim_config = copy.deepcopy(optim_config)
+    train_parameters = filter(lambda p: p.requires_grad, model.parameters() )
+    optim = getattr(torch.optim, optim_config.pop('name'))(train_parameters, **optim_config)
+    
+    # step2: 学习率相关的配置
+    lr_scheduler_config = copy.deepcopy(lr_scheduler_config)
+    lr_scheduler_name = lr_scheduler_config.pop('name')
+    lr_scheduler = getattr(lr, lr_scheduler_name)(lr_scheduler_config, optim)
+    
+    
+    
+    
+    # step2: 学习率相关的配置
+
---
+++ train_utility/optimizer/lr.py
@@ -0,0 +1,67 @@
+import math,copy
+from functools import partial
+from torch.optim import lr_scheduler
+
+class StepLR:
+    def __init__(self, 
+                 warmup_epoch=0, 
+                 gamma=0.9,
+                 last_epoch=-1,
+                 **kwargs   
+                 ):
+        super(StepLR, self).__init__()
+        self.lr_config = copy.deepcopy(kwargs)
+        self.last_epoch = last_epoch
+        self.warmup_epoch = warmup_epoch
+        self.gamma = gamma
+        self.initial_lr = self.lr_config.pop('lr')  # 初始学习率 
+        self.warpup_init_lr = self.lr_config.get('warpup_init_lr', 0)  # 初始学习率 (同时也是warmup的目标学习率)
+        self.warpup_target_lr = self.lr_config.get('warpup_target_lr', 0)
+        
+    def __call__(self, optimizer):
+        #  每次执行 lr_scheduler_.step() 时，last_epoch =+ 1, 并将 last_epoch 传入 lambda_func
+        lr_scheduler_ = lr_scheduler.LambdaLR(optimizer, self.lambda_func, last_epoch=self.last_epoch)
+        return lr_scheduler_
+    
+    def lambda_func(self, epoch):
+        if epoch < self.warmup_epoch:
+            return (self.warpup_init_lr + (self.warpup_target_lr - self.warpup_init_lr) * epoch / max(1,self.warmup_epoch)) / self.warpup_target_lr
+            # return 1.0 * (epoch+1) / max(1,self.warmup_epoch)
+        else:
+            return self.gamma ** epoch
+        
+        
+        
+if __name__ == '__main__':
+    lr_config = {
+        'lr': 0.001,
+        'warmup_epoch': 2,
+        'warpup_init_lr': 0.0001,
+        'warpup_target_lr': 0.001,
+        "NUM_EPOCHS": 100
+    }
+    import torch
+    import torch.optim as optim
+    import torch.nn as nn
+    import matplotlib.pyplot as plt
+
+    def schedular_lr(optimizer, schedular):
+        lr_history = []
+        epoch_history = []
+        """optimizer的更新在scheduler更新的前面"""
+        for epoch in range(lr_config["NUM_EPOCHS"]):
+            optimizer.step() # 更新参数
+            lr_history.append(optimizer.param_groups[0]['lr'])
+            schedular.step() # 调整学习率
+        plt.plot(lr_history)
+        plt.show()
+        return lr_history
+    
+    model = torch.nn.Linear(10, 2)  # 示例模型
+    optimizer = optim.SGD(model.parameters(), lr=lr_config['lr'])
+    lr_scheduler_ = StepLR(**lr_config)(optimizer)
+    
+
+    
+    lr_l = schedular_lr(optimizer, lr_scheduler_)
+    # print(lr_l)
